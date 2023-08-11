dd67f6165d658153a6bd71383ba6ee8b90ec8b96
Elrapido001 <jimmyifeanyi@yahoo.com>
2023-08-11 11:33:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-~~~~~*return: alwau 0(success)
-*/
+ * main - entry point
+ * returns: always 0 (success)
+ */
+/* This program uses the puts function to print a message */
 int main(void)
-'{
-~~~puts ("Programming is like building a multilingual puzzle,");
+{
+puts("Programming is like building a multilingual puzzle,");
 return (0);
-}'
+}
 
