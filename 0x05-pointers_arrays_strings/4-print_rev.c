48b78fabadabb607082f499873511df3ea15310d
Elrapido001 <jimmyifeanyi@yahoo.com>
2023-08-21 17:29:00 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,6 @@ void print_rev(char *s)
 {
 int longi = 0;
 int o;
-
 while (*s != '\0')
 {
 longi++;
@@ -16,10 +15,9 @@ s++;
 }
 s--;
 for (o = longi; o > 0; o--)
+{
 _putchar(*s);
 s--;
 }
-
 _putchar('\n');
 }
-
